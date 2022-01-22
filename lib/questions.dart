597e2c68563ca1eeca3a5ec065fc07b259693944
Prashanth BS <bsprashanth26@gmail.com>
2022-01-22 13:48:06 +0530

    Initial commit

---
+++ lib/questions.dart
@@ -0,0 +1,13 @@
+import 'package:quizapp/question.dart';
+
+class Questions {
+  List<Question> questionBank = [
+    Question(questionText: "Question1", answer: true),
+    Question(questionText: "Question2", answer: true),
+    Question(questionText: "Question3", answer: false),
+    Question(questionText: "Question4", answer: true),
+    Question(questionText: "Question5", answer: false),
+    Question(questionText: "Question6", answer: false),
+    Question(questionText: "Question7", answer: false),
+  ];
+}
